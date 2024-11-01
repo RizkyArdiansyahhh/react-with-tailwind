2f2a86077d46869b07f11d71765d31cf2e929b0e
RizkyArdiansyahhh <rizky.ardiansyahhh06@gmail.com>
2024-11-02 02:09:02 +0700

    instalasi react with vite and tailwind css

---
+++ react-app/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="flex justify-center min-h-screen items-center bg-slate-500 font-bold">
+        Hello world
+      </h1>
+    </>
+  );
+}
+
+export default App;
