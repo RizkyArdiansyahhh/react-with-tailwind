ae88d496851c36ce675cdd71c1d1de0c55193b4d
RizkyArdiansyahhh <rizky.ardiansyahhh06@gmail.com>
2024-11-02 02:36:38 +0700

    learn props in react

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,10 +1,12 @@
-const ButtonRed = () => {
+/* eslint-disable react/prop-types */
+const Button = (props) => {
+  const { warna = "bg-slate-600", children = "..." } = props;
   return (
     <button
-      className="h-10 px-6 font-semibold rounded-md bg-red-500 text-white"
+      className={`h-10 px-6 font-semibold rounded-md ${warna} text-white`}
       type="submit"
     >
-      Buy now
+      {children}
     </button>
   );
 };
@@ -13,9 +15,9 @@ function App() {
   return (
     <>
       <div className="flex justify-center min-h-screen gap-x-3 items-center bg-sky-300">
-        <ButtonRed></ButtonRed>
-        <ButtonRed></ButtonRed>
-        <ButtonRed></ButtonRed>
+        <Button warna="bg-red-500">Login</Button>
+        <Button warna="bg-lime-500">Logout</Button>
+        <Button></Button>
       </div>
     </>
   );
