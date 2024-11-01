11ab8d8943ef4d8dd3bec666ed28746e09ca546e
RizkyArdiansyahhh <rizky.ardiansyahhh06@gmail.com>
2024-11-02 02:19:10 +0700

    learn component

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,11 +1,23 @@
+const ButtonRed = () => {
+  return (
+    <button
+      className="h-10 px-6 font-semibold rounded-md bg-red-500 text-white"
+      type="submit"
+    >
+      Buy now
+    </button>
+  );
+};
+
 function App() {
   return (
     <>
-      <h1 className="flex justify-center min-h-screen items-center bg-slate-500 font-bold">
-        Hello world
-      </h1>
+      <div className="flex justify-center min-h-screen gap-x-3 items-center bg-sky-300">
+        <ButtonRed></ButtonRed>
+        <ButtonRed></ButtonRed>
+        <ButtonRed></ButtonRed>
+      </div>
     </>
   );
 }
-
 export default App;
