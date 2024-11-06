6ca46b3babdec78eefbbabc665cd29aad70fd200
RizkyArdiansyahhh <rizky.ardiansyahhh06@gmail.com>
2024-11-06 23:01:02 +0700

    rendering list

--- react-app/src/Pages/products.jsx
+++ react-app/src/Pages/products.jsx
@@ -1,48 +1,43 @@
 import ProductCard from "../components/Fragments/ProductCard";
 
+const products = [
+  {
+    id: 1,
+    title: "Sepatu",
+    image: "/image/product-1.jpg",
+    description: `
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Assumenda veritatis, fugiat dolores vel aperiam et eum odio dicta! Iste hic sit modi amet magnam molestiae perspiciatis eaque cumque nam dolorum.`,
+    price: "Rp. 1.000.000",
+  },
+  {
+    id: 2,
+    title: "Sepatu",
+    image: "/image/product-1.jpg",
+    description: `
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Assumenda veritatis, fugiat dolores vel aperiam et eum odio dicta! Iste hic sit modi amet magnam molestiae perspiciatis eaque cumque nam dolorum.`,
+    price: "Rp. 1.000.000",
+  },
+  {
+    id: 3,
+    title: "Sepatu",
+    image: "/image/product-1.jpg",
+    description: `
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Assumenda veritatis, fugiat dolores vel aperiam et eum odio dicta! Iste hic sit modi amet magnam molestiae perspiciatis eaque cumque nam dolorum.`,
+    price: "Rp. 1.000.000",
+  },
+];
 const ProductsPage = () => {
   return (
     <div className="flex justify-center gap-x-6 mt-5">
-      <ProductCard>
-        <ProductCard.Header image="/image/product-1.jpg" />
-        <ProductCard.Body title="Sepatu">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel commodi
-          iusto, quasi odit alias autem reiciendis ullam explicabo. Ipsum sint
-          ad rem doloribus aliquam aspernatur quidem enim repudiandae suscipit
-          labore?
-        </ProductCard.Body>
-        <ProductCard.Footer price="Rp. 1.000.000" />
-      </ProductCard>
-      <ProductCard>
-        <ProductCard.Header image="/image/product-1.jpg" />
-        <ProductCard.Body title="Sepatu">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel commodi
-          iusto, quasi odit alias autem reiciendis ullam explicabo. Ipsum sint
-          ad rem doloribus aliquam aspernatur quidem enim repudiandae suscipit
-          labore?
-        </ProductCard.Body>
-        <ProductCard.Footer price="Rp. 1.000.000" />
-      </ProductCard>
-      <ProductCard>
-        <ProductCard.Header image="/image/product-1.jpg" />
-        <ProductCard.Body title="Sepatu">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel commodi
-          iusto, quasi odit alias autem reiciendis ullam explicabo. Ipsum sint
-          ad rem doloribus aliquam aspernatur quidem enim repudiandae suscipit
-          labore?
-        </ProductCard.Body>
-        <ProductCard.Footer price="Rp. 1.000.000" />
-      </ProductCard>
-      <ProductCard>
-        <ProductCard.Header image="/image/product-1.jpg" />
-        <ProductCard.Body title="Sepatu">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel commodi
-          iusto, quasi odit alias autem reiciendis ullam explicabo. Ipsum sint
-          ad rem doloribus aliquam aspernatur quidem enim repudiandae suscipit
-          labore?
-        </ProductCard.Body>
-        <ProductCard.Footer price="Rp. 1.000.000" />
-      </ProductCard>
+      {products.map((product) => (
+        <ProductCard key={product.id}>
+          <ProductCard.Header image={product.image} />
+          <ProductCard.Body title={product.title}>
+            {product.description}
+          </ProductCard.Body>
+          <ProductCard.Footer price={product.price} />
+        </ProductCard>
+      ))}
     </div>
   );
 };
--- react-app/src/components/Fragments/ProductCard.jsx
+++ react-app/src/components/Fragments/ProductCard.jsx
@@ -4,7 +4,7 @@ import Button from "../Elements/Button";
 const ProductCard = (props) => {
   const { children } = props;
   return (
-    <div className="w-full max-w-sm bg-gray-800 border border-gray-700 rounded-lg shadow">
+    <div className="w-full max-w-sm bg-gray-800 border border-gray-700 rounded-lg shadow flex flex-col">
       {children}
     </div>
   );
@@ -22,7 +22,7 @@ const Header = (props) => {
 const Body = (props) => {
   const { children, title } = props;
   return (
-    <div className="px-5 pb-5">
+    <div className="px-5 pb-5 h-full">
       <a href="">
         <h5 className="text-xl font-semibold tracking-tight text-white">
           {title}
